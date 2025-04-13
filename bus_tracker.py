1f0f7cf0844991b375c6b8316c51f5c8ef9d2cd6
ARAVIND.K.R <154773786+aravindkr-dev@users.noreply.github.com>
2025-04-13 14:20:00 +0530

    added authentication and dashboard , still needed to change the signup and login page

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, render_template, jsonify, session
+from flask import Flask, request, render_template, jsonify, session, redirect , url_for
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sympy.codegen.ast import continue_
 
-
-usera = None
 
 app = Flask('__name__'  , template_folder='templates')
 user_locations = {}
@@ -12,6 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./abcd.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+app.secret_key = 'secret'
 
 class Mytable(db.Model):
     __tablename__ = 'user_cred'
@@ -30,47 +32,49 @@ class Mytable(db.Model):
         }
 
 
+def verify():
+    if 'username' not in session:
+        print("User Not Logged In")
+        return False
+    return True
+
+
 
 @app.route('/')
-def index():
-    return render_template('user.html' , user_id = usera)
+def dashboard():
+    v = verify()
+    if v:
+        return render_template('dashboard.html' , user_id = session.get('username'))
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
 
-"""@app.route('/location', methods=['POST'])
-def receive_location():
-    data = request.json
-    user_id = data.get('user_id', 'anonymous')
-    lat = data['latitude']
-    lon = data['longitude']
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    if user_id not in user_locations:
-        user_locations[user_id] = {
-            'trail': [],
-            'color': '#' + ''.join([hex(ord(c) % 16)[2:] for c in user_id[:3]])
-        }
 
-    user_locations[user_id]['trail'].append({
-        'lat': lat,
-        'lon': lon,
-        'timestamp': timestamp
-    })
 
-    if len(user_locations[user_id]['trail']) > 20:
-        user_locations[user_id]['trail'] = user_locations[user_id]['trail'][-20:]
+@app.route('/bus')
+def bus():
+    if not verify() and session['role'] != 'bus':
+        return redirect(url_for('login'))
+    return render_template('bus.html')
 
-    return 'Location received'"""
+@app.route('/view_bus')
+def view_bus():
+    if not verify() or session.get('role') == 'student':
+        return redirect(url_for('login'))
+    return render_template('viewbus.html')
 
 @app.route('/locations')
 def get_locations():
+    if not verify():
+        return redirect(url_for('login'))
     return jsonify(user_locations)
 
 
 @app.route('/location', methods=['POST'])
 def receive_location():
+    if not verify():
+        return redirect(url_for('login'))
     data = request.json
     user_id = data.get('user_id', 'anonymous')
     lat = data['latitude']
@@ -109,7 +113,6 @@ def signup():
 @app.route('/login' , methods = ['GET' , 'POST'])
 
 def login():
-    global usera
     if request.method == 'POST':
         user = request.form['username']
         passw = request.form['password']
@@ -118,11 +121,14 @@ def login():
         print(cred)
         for x in cred:
             if x['name'] == user and x['passw'] == passw and x['role'] == 'student':
-                usera = x['name']
-                return 'ok'
+                session['username'] = x['name']
+                session['role'] = x['role']
+                return redirect(url_for('dashboard'))
             elif x['name'] == user and x['passw'] == passw and x['role'] == 'admin':
+                session['username'] = x['name']
+                session['role'] = x['role']
                 return 'admin'
-        return 'not ok'
+        return redirect(url_for('view_bus'))
     elif request.method == 'GET':
         return render_template('login.html')
 
@@ -132,4 +138,4 @@ if __name__ == '__main__':
     """ app.app_context().push()"""
     with app.app_context():
         db.create_all()
-    app.run(host = '0.0.0.0' , debug=True)
+    app.run(host = '0.0.0.0' , debug=True , port = 5000)
